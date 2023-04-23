f44661a099ed86a4a160b19b8a7d39146cce7dd9
David <david.betancur.ramirez.dbr@gmail.com>
2023-04-23 16:51:34 -0500

    first commit

---
+++ Caballo.java
@@ -0,0 +1,71 @@
+package Caballo;
+
+import java.util.Stack;
+
+public class Caballo {
+	private int n;
+	private int[][] tablero;
+	private int[][] posiblesPasos = { { 2, 1 }, { 1, 2 }, { -1, 2 }, { -2, 1 }, { -2, -1 }, { -1, -2 }, { 1, -2 }, { 2, -1 } };
+	private Stack<Integer[]> pilita = new Stack<Integer[]>();
+
+	public Caballo(int n) {
+		this.n = n;
+		this.tablero = new int[n][n];
+	}
+
+	public boolean resolver() {
+		return resolver(0, 0);
+	}
+	public boolean resolver(int fila, int columna) {
+		return resolver(fila, columna, 1);
+	}
+	private boolean resolver(int fila, int columna, int paso) {
+		tablero[fila][columna] = paso;
+		Integer[] algo = { fila, columna };
+		pilita.push(algo);
+		
+		if (paso == n * n)
+			return true; // Ya se visitaron todas las casillas
+		
+		boolean encontrado = false;
+		int i = 0;
+		
+		while (i < 8 && !encontrado) {
+			int nuevaFila = fila + posiblesPasos[i][0];
+			int nuevaColumna = columna + posiblesPasos[i][1];
+			if (esValida(nuevaFila, nuevaColumna))
+				encontrado = resolver(nuevaFila, nuevaColumna, paso + 1);
+			i++;
+		}
+		
+		if (!encontrado) {
+			tablero[fila][columna] = 0;
+			pilita.pop();
+		}
+		
+		return encontrado;
+	}
+
+	private boolean esValida(int fila, int columna) {
+		return !(fila < 0 || fila >= n || columna < 0 || columna >= n || tablero[fila][columna] != 0);
+	}
+
+	public void imprimir() {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++)
+				System.out.print(tablero[i][j] + " ");
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+		Caballo caballo = new Caballo(8);
+		if (caballo.resolver()) {
+			caballo.imprimir();
+			System.out.println("");
+		} else {
+			System.out.println("No se encontró una solución");
+		}
+	}
+
+}
