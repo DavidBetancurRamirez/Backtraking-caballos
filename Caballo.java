f2f6e2a8374f1b09c43062a0353c52e18cabe9de
David <david.betancur.ramirez.dbr@gmail.com>
2023-04-25 19:32:28 -0500

    Stack de movimientos

--- Caballo.java
+++ Caballo.java
@@ -5,27 +5,33 @@ import java.util.Stack;
 
 public class Caballo {
 	private int n;
-	private int[][] tablero;
+	private int[][] tablero, movimiento;
 	private final int[][] posiblesPasos = {{ 2, 1 },{ 1, 2 },{ -1, 2 },{ -2, 1 },{ -2, -1 },{ -1, -2 },{ 1, -2 },{ 2, -1 }};
 	private Stack<int[][]> tableros = new Stack<int[][]>();
+	private Stack<int[][]> movimientos = new Stack<int[][]>();
 	
 	public Caballo(int n) {
 		this.n = n;
 		this.tablero = new int[n][n];
+		this.movimiento = new int[n*n][2];
 	}
 
 	public boolean resolver() {
 		return resolver(0, 0, 1);
 	}
 	public boolean resolver(int fila, int columna, int cantidadSoluciones) {
-		return resolver(fila, columna, 1, cantidadSoluciones);
+		return (cantidadSoluciones!=0) ? resolver(fila, columna, 1, cantidadSoluciones) : false;
 	}
 	private boolean resolver(int fila, int columna, int paso, int cantidadSoluciones) {
 		tablero[fila][columna] = paso;
+		int[] pasito = {fila,columna};
+		movimiento[paso-1] = pasito;
 		
 		if (paso == n * n) {
-			int[][] copiaMatriz = copiaMatriz(tablero);
-			tableros.push(copiaMatriz);
+			int[][] copiaTablero = copiaMatriz(tablero);
+			int[][] copiaMovimiento = copiaMatriz(movimiento);
+			tableros.push(copiaTablero);
+			movimientos.push(copiaMovimiento);
 			
 			if (tableros.size()==cantidadSoluciones)
 				return true;		
@@ -45,8 +51,11 @@ public class Caballo {
 			i++;
 		}
 		
-		if (!encontrado)
+		if (!encontrado) {
 			tablero[fila][columna] = 0;
+			int[] pasitoRemovido = {0,0};
+			movimiento[paso-1] = pasitoRemovido;
+		}
 		
 		return encontrado;
 	}
@@ -71,9 +80,9 @@ public class Caballo {
 	}
 	
 	public int[][] copiaMatriz(int[][] original) {
-		int[][] nueva = new int[original.length][original.length];
+		int[][] nueva = new int[original.length][original[0].length];
 		for (int i = 0; i < nueva.length; i++) {
-			for (int j = 0; j < nueva.length; j++)
+			for (int j = 0; j < nueva[0].length; j++)
 				nueva[i][j] = original[i][j];
 		}
 		
@@ -84,6 +93,7 @@ public class Caballo {
 		Caballo caballo = new Caballo(8);
 		caballo.resolver(0,0,3);
 		caballo.imprimir();
+		System.out.println("");
 	}
 
 }
